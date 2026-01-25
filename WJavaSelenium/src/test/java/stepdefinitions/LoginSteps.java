ad13c4111456d2491068c74d0eca28482e32c358
Akash <dany.mkm1@gmail.com>
2026-01-25 21:02:19 +0530

    add basic framework structure

---
+++ WJavaSelenium/src/test/java/stepdefinitions/LoginSteps.java
@@ -0,0 +1,26 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginSteps {
+    WebDriver driver=new ChromeDriver();
+
+    @Given("user is on login page")
+    public void user_is_on_login_page() {
+        System.out.println("Navigate to google page");
+        driver.get("https://www.google.com/");
+        System.out.println("Navigate to login page");
+    }
+
+    @When("user enters valid credentials")
+    public void user_enters_valid_credentials() {
+        System.out.println("Enter credentials");
+    }
+
+    @Then("user should land on dashboard")
+    public void user_should_land_on_dashboard() {
+        System.out.println("Verify dashboard");
+    }
+}
